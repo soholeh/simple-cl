0850c3d749a98d796b3353ee8979d7e38dc62309
soholeh <msolehudin130998@gmail.com>
2020-03-02 22:23:20 +0700

    Initial commit

---
+++ app/src/main/java/muhammad/solehudin/clpert5/MainActivity.kt
@@ -0,0 +1,26 @@
+package muhammad.solehudin.clpert5
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        imageButton2.setOnClickListener({
+            Toast.makeText(this, "Thanks!",Toast.LENGTH_LONG).show()
+        })
+
+        button2.setOnClickListener({
+            Toast.makeText(this, "Soon!",Toast.LENGTH_LONG).show()
+        })
+
+        button.setOnClickListener({
+            Toast.makeText(this, "Yeahhh!",Toast.LENGTH_LONG).show()
+        })
+    }
+}
