1af5c1f68dec4355b80afe00fa9ef0f26ca90a3c
soholeh <msolehudin130998@gmail.com>
2020-03-03 06:50:43 +0700

    change fav

--- app/src/main/java/muhammad/solehudin/clpert5/MainActivity.kt
+++ app/src/main/java/muhammad/solehudin/clpert5/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        imageButton2.setOnClickListener({
+        imageView2.setOnClickListener({
             Toast.makeText(this, "Thanks!",Toast.LENGTH_LONG).show()
         })
 
